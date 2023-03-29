9958f8c033d65d5392834963ec39945c926c1189
GuileSoler87 <guillermosoler87@gmail.com>
2023-03-29 09:36:05 +0200

    corrección direcciones para funcionamiento correcto en postman

--- routes/products.js
+++ routes/products.js
@@ -9,7 +9,7 @@ router.get('/products',productcontroller.getAll);
 router.get('/prodcat',productcontroller.getProdCat);
 router.get('/products/:id',productcontroller.getById);
 router.get('/prods/descend',productcontroller.getByPriDesc);
-router.get('/products/search/:name',productcontroller.getProdName);
+router.get('/search/:name',productcontroller.getProdName);
 router.delete('/products/:id',productcontroller.deleteById);
 
 module.exports = router;
